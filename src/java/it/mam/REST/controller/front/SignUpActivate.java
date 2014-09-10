e100ef4c8a79163106d29cf8215c99e0626fa367
Krave90 <rumble90@hotmail.it>
2014-09-10 22:23:25 +0200

    Aggiunte Servlet LogIn e Registrazione

---
+++ src/java/it/mam/REST/controller/front/SignUpActivate.java
@@ -0,0 +1,39 @@
+
+package it.mam.REST.controller.front;
+
+import it.mam.REST.controller.RESTBaseController;
+import it.univaq.f4i.iw.framework.result.FailureResult;
+import it.univaq.f4i.iw.framework.result.TemplateResult;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mirko
+ */
+public class SignUpActivate extends RESTBaseController {
+
+ // prende il template di default di errore e e ci stampa il messaggio passato come parametro
+    private void action_error(HttpServletRequest request, HttpServletResponse response, String message) {
+        FailureResult fail = new FailureResult(getServletContext());
+        fail.activate(message, request, response);
+    }
+    
+    private void action_activate_signup(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    TemplateResult result = new TemplateResult(getServletContext());
+        request.setAttribute("outline_tpl", null);
+        result.activate("signUp.ftl.html", request, response);
+    }
+        
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        try {
+            action_activate_signup(request, response);
+        } catch (IOException ex) {
+            action_error(request, response, ex.getMessage());
+        }
+    }
+}
