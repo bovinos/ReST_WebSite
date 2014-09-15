99a267d5e057a4803de13b2b1818069452cbc8f6
Krave90 <rumble90@hotmail.it>
2014-09-15 13:32:10 +0200

    Modifica Servlet

--- src/java/it/mam/REST/controller/front/SeriesList.java
+++ src/java/it/mam/REST/controller/front/SeriesList.java
@@ -1,12 +1,19 @@
 package it.mam.REST.controller.front;
 
 import it.mam.REST.controller.RESTBaseController;
+import it.mam.REST.data.model.Channel;
+import it.mam.REST.data.model.Genre;
+import it.mam.REST.data.model.Series;
 import it.mam.REST.data.model.User;
+import it.mam.REST.data.model.UserSeries;
+import it.mam.REST.utility.RESTSortLayer;
 import it.univaq.f4i.iw.framework.result.FailureResult;
 import it.univaq.f4i.iw.framework.result.SplitSlashesFmkExt;
 import it.univaq.f4i.iw.framework.result.TemplateResult;
 import it.univaq.f4i.iw.framework.security.SecurityLayer;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,7 +33,6 @@ public class SeriesList extends RESTBaseController {
     // prende tutte le serie e le passa al template seriesList.ftl.html
     private void action_series_list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         TemplateResult result = new TemplateResult(getServletContext());
-
         request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
         request.setAttribute("series", getDataLayer().getSeries());
         //Controllo la sessione e creo l'utente
@@ -36,9 +42,85 @@ public class SeriesList extends RESTBaseController {
         User user = getDataLayer().getUser(SecurityLayer.checkNumeric((request.getSession().getAttribute("userid")).toString()));
         request.setAttribute("user", user);
         }
+        request.setAttribute("genres", getDataLayer().getGenres());
+        request.setAttribute("channels", getDataLayer().getChannels());
         result.activate("seriesList.ftl.html", request, response);
     }
 
+    private void action_order_series_list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+        String[] series = request.getParameterValues("series");
+        List<Series> seriesList = new ArrayList();
+        for(String s: series){
+          seriesList.add(getDataLayer().getSeries(SecurityLayer.checkNumeric(s)));
+        }
+        int ordertype = SecurityLayer.checkNumeric(request.getParameter("o"));
+        switch(ordertype) {
+            case 1: RESTSortLayer.sortSeriesByName(seriesList);
+            break;
+            case 2: RESTSortLayer.sortSeriesByPopularity(seriesList);
+            break;
+            case 3: RESTSortLayer.sortSeriesByRating(seriesList);
+        }
+    }
+    
+    private void action_filter_series_list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+        List<Series> seriesList = getDataLayer().getSeries();
+        
+        //Filtro Serie per Nome
+        if(request.getParameter("filtername") != null){
+            for(Series s: seriesList){
+                if(!(s.getName().equals(request.getParameter("filtername")))){
+                    seriesList.remove(s);
+                }
+            }
+        }
+        //Filtro Serie per genere
+        if(request.getParameterValues("filtergenre") != null){
+            List<Genre> genresList = new ArrayList();
+            for(String g: request.getParameterValues("filtergenre")){
+                genresList.add(getDataLayer().getGenre(SecurityLayer.checkNumeric(g)));
+            }
+            for(Series s: seriesList){
+                for(Genre g: genresList){
+                if(!(s.getGenres().contains(g))){
+                    seriesList.remove(s);
+                }
+                }
+            }
+        }
+        
+                //Filtro Serie per Nome
+        if(request.getParameter("filtermyseries") != null){
+            TemplateResult result = new TemplateResult(getServletContext());
+            if(SecurityLayer.checkSession(request) == null) result.activate("logIn.ftl.html", request, response);
+            User user =getDataLayer().getUser(SecurityLayer.checkNumeric((String)request.getSession().getAttribute("userid")));
+            List<UserSeries> usList = getDataLayer().getUserSeries(user);
+            for(Series s: seriesList){
+                for(UserSeries us: usList){
+              //  if(!()
+              //          {
+               //     seriesList.remove(s);
+             //   }
+                }
+            }
+        }
+        
+          if(request.getParameterValues("filterchannel") != null){
+            List<Channel> channelList = new ArrayList();
+            for(String c: request.getParameterValues("filterchannel")){
+                channelList.add(getDataLayer().getChannel(SecurityLayer.checkNumeric(c)));
+            }
+            for(Series s: seriesList){
+                for(Channel c: channelList){
+             //   if(!(s.get?????????().contains(c))){ ///////////////////////////// Che get?? non ci sta getChannel per la serie!!!
+                //    seriesList.remove(s);
+               // }
+                }
+            }
+        }
+    }
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         try {
---
+++ src/java/it/mam/REST/utility/RESTSortLayer.java
@@ -0,0 +1,56 @@
+package it.mam.REST.utility;
+
+import it.mam.REST.data.model.Series;
+import it.mam.REST.data.model.UserSeries;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ * @author Mirko
+ */
+public class RESTSortLayer {
+    
+    public static List<Series> sortSeriesByName(List<Series> seriesList){
+            seriesList.sort(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((Series)o1).getName().compareToIgnoreCase(((Series)o1).getName());
+            }
+        });
+        return seriesList;
+    }
+    
+    public static List<Series> sortSeriesByRating(List<Series> seriesList){
+            seriesList.sort(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return (getMediumRating((Series)o1) > getMediumRating((Series)o2)? 1 
+                        : getMediumRating((Series)o1) == getMediumRating((Series)o2)? 0: -1);
+            }
+        });
+        return seriesList;
+    }
+    
+        public static List<Series> sortSeriesByPopularity(List<Series> seriesList){
+            seriesList.sort(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return (((Series)o1).getAddCount() > ((Series)o2).getAddCount()? 1 
+                        : ((Series)o1).getAddCount() == ((Series)o2).getAddCount()? 0: -1);
+            }
+        });
+        return seriesList;
+    }
+    
+    private static float getMediumRating (Series s){
+    List<UserSeries> usList = s.getUserSeries();
+    int count = 0;
+    int ratingsum = 0;
+    for(UserSeries us: usList){
+        ratingsum += Integer.parseInt(us.getRating());
+        count++;
+    }
+    return ((float)ratingsum/count);
+    }
+}
