610bb3de4bad90e27d22d61309a34d91cc1b90ef
Krave90 <rumble90@hotmail.it>
2014-09-16 13:38:05 +0200

    xD

--- src/java/it/mam/REST/controller/front/NewsCard.java
+++ src/java/it/mam/REST/controller/front/NewsCard.java
@@ -2,12 +2,15 @@
 package it.mam.REST.controller.front;
 
 import it.mam.REST.controller.RESTBaseController;
+import it.mam.REST.data.model.Comment;
+import it.mam.REST.data.model.News;
 import it.mam.REST.data.model.User;
 import it.univaq.f4i.iw.framework.result.FailureResult;
 import it.univaq.f4i.iw.framework.result.SplitSlashesFmkExt;
 import it.univaq.f4i.iw.framework.result.TemplateResult;
 import it.univaq.f4i.iw.framework.security.SecurityLayer;
 import java.io.IOException;
+import java.util.Calendar;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,11 +29,11 @@ public class NewsCard extends RESTBaseController{
     }
     
     // prende tutte le informazioni su una news e le passa al template newsCard.ftl.html
-    private void action_news_info(HttpServletRequest request, HttpServletResponse response, int id) throws ServletException, IOException {
+    private void action_news_info(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         TemplateResult result = new TemplateResult(getServletContext());
         request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
-        request.setAttribute("news", getDataLayer().getNews(id));
+        request.setAttribute("news", getDataLayer().getNews(SecurityLayer.checkNumeric(request.getParameter("id"))));
         //Controllo che la sessione attuale sia ancora valida
         if (SecurityLayer.checkSession(request) != null){
             try{
@@ -43,13 +46,42 @@ public class NewsCard extends RESTBaseController{
         result.activate("newsCard.ftl.html", request, response);
     }
 
+    private void action_comment_news (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try{
+        TemplateResult result = new TemplateResult(getServletContext());
+        if(SecurityLayer.checkSession(request) == null){ result.activate("logIn.ftl.html", request, response); }
+        User user = getDataLayer().getUser(SecurityLayer.checkNumeric((request.getSession().getAttribute("userid")).toString()));
+        News news = getDataLayer().getNews(SecurityLayer.checkNumeric(request.getParameter("nid")));
+        Calendar c = Calendar.getInstance();
+        String title = request.getParameter("commentTitle");
+        String text = request.getParameter("commentText");
+        Comment comment = getDataLayer().createComment();
+        comment.setTitle(title);
+        comment.setText(text);
+        comment.setUser(user);
+        comment.setDate(c.getTime());
+        comment.setNews(news);
+        getDataLayer().storeComment(comment);
+        response.sendRedirect("SchedaNews?id=" + news.getID());
+        }catch (NumberFormatException ex){
+            action_error(request, response, "Field Error");
+        }
+    }
+    
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        if(request.getParameter("ncs") != null){
         try {
-            int id = SecurityLayer.checkNumeric(request.getParameter("id"));
-            action_news_info(request, response, id);
+            action_comment_news(request, response);
         } catch (IOException | NumberFormatException ex) {
             action_error(request, response, "Errore caricamento dati");
+        }
+        } else {
+            try {
+            action_news_info(request, response);
+        } catch (IOException | NumberFormatException ex) {
+            action_error(request, response, "Errore caricamento dati");
+        }
         }
     }
 
